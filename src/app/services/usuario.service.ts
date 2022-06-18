1fadb66bc58d2a42ad673778c103da756743dfdc
Yosimar Mendez <50188885+kingyosi15@users.noreply.github.com>
2022-06-17 19:14:37 -0500

    Primer commit sin effects

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private URL = 'https://reqres.in/api';
+
+  constructor( private http: HttpClient) { }
+
+
+  getUsers() {
+    return this.http.get(`${this.URL}/users?per_page=6`)
+      .pipe(
+        map( resp => resp['data'] )
+      );
+  }
+
+}
